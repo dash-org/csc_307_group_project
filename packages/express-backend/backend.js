b55875cdfebb78cbac9a12250faa0434edbee9d7
DashP06 <dashzoop@gmail.com>
2025-10-30 13:56:28 -0700

    Revert "kitchen services and endpoints"
    This reverts commit e59c2659c8da5a80ff59823ecc8f555191da33c6.

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -7,7 +7,6 @@ import connectionString from './secret.js';
 import memberServices from './services/member-services.js';
 import inventoryServices from './services/inventory-services.js';
 import itemServices from './services/item-services.js';
-import kitchenServices from './services/kitchen-services.js';
 
 mongoose.set('debug', true);
 
@@ -78,22 +77,7 @@ app.get('/items', (req, res) => {
     .then((items) => {
       return res.send({ item_list: items });
     })
-    .catch((error) => console.log(error));
-});
-
-app.get('/kitchens', (req, res) => {
-  kitchenServices
-    .getKitchen(
-      req.query.name,
-      req.query.owner,
-      req.query.createdAt,
-      req.query.inventories ? req.query.inventories.split(',') : [],
-      req.query.memberships ? req.query.memberships.split(',') : []
-    )
-    .then((kitchens) => {
-      return res.send({ kitchen_list: kitchens });
-    })
-    .catch((error) => console.log(error));
+    .cath((error) => console.log(error));
 });
 
 app.get('/users/:id', (req, res) => {
@@ -148,20 +132,6 @@ app.get('/inventories/:id', (req, res) => {
     });
 });
 
-app.get('/kitchens/:id', (req, res) => {
-  const id = req.params['id']; //or req.params.id
-
-  kitchenServices
-    .findKitchenById(id)
-    .then((kitchen) => {
-      res.send(kitchen);
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(404).send('Resource not found.');
-    });
-});
-
 app.post('/users', (req, res) => {
   let userToAdd = req.body;
   userServices
@@ -202,17 +172,6 @@ app.post('/inventories', (req, res) => {
     .then((inventory) => {
       inventoryToAdd = inventory;
       res.status(201).send(inventoryToAdd);
-    })
-    .catch((error) => console.log(error));
-});
-
-app.post('/kitchens', (req, res) => {
-  let kitchenToAdd = req.body;
-  kitchenServices
-    .addKitchen(kitchenToAdd)
-    .then((kitchen) => {
-      kitchenToAdd = kitchen;
-      res.status(201).send(kitchenToAdd);
     })
     .catch((error) => console.log(error));
 });
@@ -265,20 +224,6 @@ app.delete('/inventories/:id', (req, res) => {
     .deleteInventoryById(id)
     .then((inventory) => {
       console.log(`Deleted inventory ${inventory.id}`);
-      res.status(204).send();
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(404).send();
-    });
-});
-
-app.delete('/kitchens/:id', (req, res) => {
-  const id = req.params['id'];
-  kitchenServices
-    .deleteKitchenById(id)
-    .then((kitchen) => {
-      console.log(`Deleted kitchen ${kitchen.id}`);
       res.status(204).send();
     })
     .catch((error) => {
