2d8fc3292799748be5561da7e0599b7d57273df2
DashP06 <dashzoop@gmail.com>
2025-10-30 13:57:34 -0700

    Revert "add hashpassword"
    This reverts commit 24bc7f8a606bcb657fec11babfc370969ec2d351.

--- packages/express-backend/schemas/user.js
+++ packages/express-backend/schemas/user.js
@@ -21,7 +21,6 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   createdAt: { type: Date, default: Date.now },
-  hashpassword: { type: String, required: true },
 });
 const User = mongoose.model('User', userSchema);
 export default User;
