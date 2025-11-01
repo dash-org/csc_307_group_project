ca0f1dfbb86fe2586662812f04309d21ce280a08
raf <rafaelsalcedoprado@gmail.com>
2025-10-31 19:00:32 -0700

    Fix: add back in line deleted from merge

--- packages/express-backend/schemas/user.js
+++ packages/express-backend/schemas/user.js
@@ -20,6 +20,7 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   createdAt: { type: Date, default: Date.now },
+  hashpassword: { type: String, required: true },
 });
 const User = mongoose.model('User', userSchema);
 export default User;
