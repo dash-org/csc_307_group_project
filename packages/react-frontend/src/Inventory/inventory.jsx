c41ed874492f7766943e3f069f7f29ff849a817a
SWoo83 <102641343+SWoo83@users.noreply.github.com>
2025-11-11 16:20:32 -0800

    boilerplate- Inventory
    bare bones version of an empty inventory

---
+++ packages/react-frontend/src/Inventory/inventory.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import calendar from "./calendar.png";
+import chat from "./chat.png";
+import controlPanel from "./control-panel.png";
+import highImportance from "./high-importance.png";
+import playlist from "./playlist.png";
+import plus from "./plus.png";
+import search from "./search.png";
+import settings from "./settings.png";
+import shoppingBag from "./shopping-bag.png";
+import "./style.css";
+import testAccount from "./test-account.png";
+
+export const InventoryEmpty = () => {
+  return (
+    <div className="inventory-empty">
+      <div className="text-wrapper">SIDER</div>
+
+      <div className="rectangle" />
+
+      <div className="div" />
+
+      <div className="rectangle-2" />
+
+      <div className="text-wrapper-2">Dashboard</div>
+
+      <div className="text-wrapper-3">Home</div>
+
+      <div className="text-wrapper-4">Lists</div>
+
+      <div className="ellipse" />
+
+      <div className="ellipse-2" />
+
+      <div className="ellipse-3" />
+
+      <div className="ellipse-4" />
+
+      <div className="ellipse-5" />
+
+      <div className="ellipse-6" />
+
+      <div className="ellipse-7" />
+
+      <img className="control-panel" alt="Control panel" src={controlPanel} />
+
+      <img className="calendar" alt="Calendar" src={calendar} />
+
+      <img className="shopping-bag" alt="Shopping bag" src={shoppingBag} />
+
+      <img className="playlist" alt="Playlist" src={playlist} />
+
+      <img className="chat" alt="Chat" src={chat} />
+
+      <img
+        className="high-importance"
+        alt="High importance"
+        src={highImportance}
+      />
+
+      <img className="test-account" alt="Test account" src={testAccount} />
+
+      <div className="rectangle-3" />
+
+      <img className="search" alt="Search" src={search} />
+
+      <div className="text-wrapper-5">Search</div>
+
+      <img className="settings" alt="Settings" src={settings} />
+
+      <div className="rectangle-4" />
+
+      <div className="text-wrapper-6">Inventory</div>
+
+      <div className="text-wrapper-7">← Dashboard</div>
+
+      <div className="rectangle-5" />
+
+      <div className="rectangle-6" />
+
+      <img className="plus" alt="Plus" src={plus} />
+
+      <div className="text-wrapper-8">Empty drawers..</div>
+
+      <div className="text-wrapper-9">Settings</div>
+
+      <div className="text-wrapper-10">Reminders</div>
+
+      <div className="text-wrapper-11">Home</div>
+
+      <div className="text-wrapper-12">Shopping List</div>
+
+      <div className="text-wrapper-13">Members</div>
+
+      <div className="text-wrapper-14">About</div>
+
+      <div className="text-wrapper-15">Chat</div>
+    </div>
+  );
+};
